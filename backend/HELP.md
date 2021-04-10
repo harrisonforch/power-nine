d3343d37e9741e33750af027bfa460adeec5b312
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-10 10:52:02 -0700

    Updated with proper maven build

--- backend/HELP.md
+++ backend/HELP.md
@@ -1,3 +1,8 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The original package name 'com.example.power-nine' is invalid and this project uses 'com.example.powernine' instead.
+
 # Getting Started
 
 ### Reference Documentation
--- backend/src/test/java/com/example/powernine/PowerNineApplicationTests.java
+++ backend/src/test/java/com/example/powernine/PowerNineApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.powernine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PowerNineApplicationTests {
 
 	@Test
 	void contextLoads() {
