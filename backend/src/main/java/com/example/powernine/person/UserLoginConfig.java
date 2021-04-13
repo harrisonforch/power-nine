b872c4d0062dc5097db795cb33353914000c4476
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-13 09:54:21 -0700

    Add user login authentication

--- backend/src/main/java/com/example/powernine/person/UserLoginConfig.java
+++ backend/src/main/java/com/example/powernine/person/UserLoginConfig.java
@@ -1,9 +1,14 @@
 package com.example.powernine.person;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,10 +20,22 @@ public class UserLoginConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .loginPage("/login")
                 .permitAll()
                 .and()
             .logout()
                 .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
