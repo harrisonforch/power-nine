943ebe83e2bff3e0f3aa3bfe5340c90777167982
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-14 09:52:51 -0700

    Small move around

--- backend/src/main/java/com/example/powernine/user/UserRepository.java
+++ backend/src/main/java/com/example/powernine/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.powernine.user;
 
-import com.example.powernine.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
