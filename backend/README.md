17931eaabf2d04c390f1379979096c05c4e44f40
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-14 10:49:17 -0700

    Add GSON dependency and begin adding Card fields

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# API documentation
+
+## Overview
+
+The `power-nine` backend consists of various authenticated endpoints
--- backend/src/main/java/com/example/powernine/card/Card.java
+++ backend/src/main/java/com/example/powernine/card/Card.java
@@ -3,12 +3,28 @@ package com.example.powernine.card;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Document(collection = "card")
 public class Card {
     @Id
     private Long id;
+    private Integer arena_id;
+    private String land;
+    private Integer mtgo_id;
+    private Integer mtgo_foil_id;
+    private ArrayList<Integer> multiverse_ids;
+    private Integer tcgplayer_id;
+    private Integer cardmarket_id;
+    private String object;
+    private Long oracle_id;
+    private String prints_search_uri;
+    private String rulings_url;
+    private String scryfall_uri;
+    private String uri;
 
-    public Long getId() {
-        return id;
+
+    public static Card fromJSON(String jsonData) {
+        return null;
     }
 }
