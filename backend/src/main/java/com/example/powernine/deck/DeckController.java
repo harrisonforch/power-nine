ba5ba82c46833542dfd020539465e778271abb36
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-16 20:34:09 -0700

    Simple get deck controller

--- backend/src/main/java/com/example/powernine/deck/DeckController.java
+++ backend/src/main/java/com/example/powernine/deck/DeckController.java
@@ -1,25 +1,28 @@
 package com.example.powernine.deck;
 
+import com.example.powernine.user.User;
+import com.example.powernine.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
 @RestController
 public class DeckController {
     @Autowired
-    private PasswordEncoder encoder;
-    @Autowired
-    private DeckRepository repository;
+    private UserRepository userRepository;
 
     @GetMapping("/decks")
     List<Deck> getDecks(Principal principal) {
-        return null;
+        User user = userRepository.findByUsername(principal.getName());
+        if (user != null) {
+            return user.getDecks();
+        }
+        return new ArrayList<>();
     }
 
 }
