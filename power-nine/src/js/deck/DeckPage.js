c454aaeb81acc21e943c4f8b29fb8062803b2ff3
Rithik Rajani <rithikrajani@gmail.com>
2021-04-22 19:55:31 -0700

    baseline deck react pages

---
+++ power-nine/src/js/deck/DeckPage.js
@@ -0,0 +1,105 @@
+import '../../css/UserPage.css';
+import React from "react";
+import requestFromAPI from "../BackendAPI";
+import DeckDisplay from "./DeckDisplay";
+import UserNavbar from "./UserNavbar";
+import user_logo from "../../static/user-logo.png";
+
+class DeckPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            noDeck: false
+            //unsure if this is true / needed
+            //fix the ternary operator syntax
+            deck: this.props.location.state !== undefined ? this.state.noDeck: true,
+            isLoaded: false,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        //don't need to do this
+        requestFromAPI("http://localhost:8080/users/login", "admin", "welcome1", "POST",
+            {username: this.state.user.username, password: this.state.user.password})
+            .then(data => {
+                this.setState({
+                    isLoaded: true,
+                    user: data,
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                })
+            })
+    }
+
+    generateTable() {
+        const tableRows = [];
+        for (let i = 0; i < this.state.user.decks.length; i += 4) {
+            tableRows.push(
+                <div className={"row"}>
+                    {this.state.user.decks.slice(i, i + 4).map(deck =>
+                        <div className={"col-3"}>
+                            <DeckDisplay deck={deck} />
+                        </div>
+                    )}
+                </div>
+            )
+            tableRows.push(<br />)
+        }
+        return tableRows;
+    }
+
+    getUserDiv() {
+        return (
+            <div className={"card user-div"}>
+                <img className={"card-img-top"} src={user_logo}  alt="User Logo"/> <br /> <br />
+                <div className={"card-body"}>
+                    <p className={"card-title"}>
+                        <b className={"b"}>Username: </b> {this.state.user.username} <br />
+                        <b className={"b"}>Name: </b> {this.state.user.firstName} {this.state.user.lastName} <br />
+                        <b className={"b"}>Email: </b> {this.state.user.email} <br />
+                    </p>
+                    <p className={"card-subtitle"}>
+                        <b className={"b"}>Number of decks: </b> {this.state.user.decks.length}
+                    </p>
+                </div>
+            </div>
+        )
+    }
+
+    getDecksDiv() {
+        return (<div className={"decks-div border rounded"}>
+            <div className={"container border-dark"}>
+                {this.generateTable()}
+            </div>
+        </div>);
+    }
+
+    render() {
+        if (!this.state.isLoaded)
+            return <div />;
+        if (this.state.error !== null) {
+            return <div>
+                Error when loading <br />
+                {this.state.error}
+            </div>;
+        }
+
+        return <div>
+            {/*Navbar*/}
+            <UserNavbar />
+            {/*Left-side image and username*/}
+            {this.getUserDiv()}
+            {/*Table of deck objects*/}
+            {this.getDecksDiv()}
+        </div>;
+
+    }
+}
+
+
+export default UserPage;
