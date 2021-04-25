17930eba198ce3d28de0dc55895cc90ced16d75e
Christopher Neely <christopher.neely1200@gmail.com>
2021-04-25 12:52:39 -0700

    Dropped off header - fix

--- power-nine/src/js/user/LoggedInUser.js
+++ power-nine/src/js/user/LoggedInUser.js
@@ -1,3 +1,6 @@
+import {Redirect} from "react-router-dom";
+import React from "react";
+
 let LoggedInUser = (function() {
     let getUser = () => {
         return JSON.parse(localStorage.getItem("user"));
